5045d40d8b2624748b20f1b7fc6cc7c49f0fccfc
Muhammad Ali <malismile786@gmail.com>
2024-10-21 17:24:04 +0500

    added backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { WebsocketModule } from './modules/websocket/websocket.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot('mongodb+srv://admin:ysEbv3wOsutF9zUl@cluster1.7xsfezu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1'),  // Connect to MongoDB
+    WebsocketModule,
+  ],
 })
 export class AppModule {}
