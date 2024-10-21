92605ca27c7466988bdb203ff378c205855cb7fa
Muhammad Ali <malismile786@gmail.com>
2024-10-21 20:10:12 +0500

    added frontend

--- backend/src/modules/websocket/gateway/websocket.gateway.ts
+++ backend/src/modules/websocket/gateway/websocket.gateway.ts
@@ -11,17 +11,18 @@ import { Server, Socket } from 'socket.io';
 import { WebsocketService } from '../services/websocket.service';
 import { CreateMessageDto } from '../dto/create-message.dto';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: { origin: 'http://localhost:3001' } })
 export class WebsocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server: Server;
 
     constructor(private readonly websocketService: WebsocketService) { }
 
     afterInit(server: Server) {
-        console.log('WebSocket Gateway Initialized');
+        console.log('WebSocket Gateway Initialized....');
     }
 
     handleConnection(client: Socket) {
+        console.log(client)
         console.log(`Client connected: ${client.id}`);
     }
 
@@ -31,7 +32,6 @@ export class WebsocketGateway implements OnGatewayInit, OnGatewayConnection, OnG
 
     @SubscribeMessage('message')
     async handleMessage(@MessageBody() createMessageDto: CreateMessageDto): Promise<void> {
-
         const processedMessage = await this.websocketService.processData(createMessageDto);
         this.server.emit('processedData', processedMessage);
     }
